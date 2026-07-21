6fe320cb66c93849871e9369dfa7808392bf62d0
pedrofintech <pedrohabraz@gmail.com>
2026-07-21 10:48:26 +0100

    Add files via upload

--- eibor.js
+++ eibor.js
@@ -10,7 +10,7 @@
   if (window.__bmcEiborLoaded) { return; }
   window.__bmcEiborLoaded = 1;
 
-  var DATA_URL = "https://cdn.jsdelivr.net/gh/pedrofintech/eibor-rate-today@main/eibor-data.json";
+  var DATA_URL = "https://raw.githubusercontent.com/pedrofintech/eibor-rate-today/main/eibor-data.json";
 
   var CSS = `/* ===================== BMC-EIBOR - styles ===================== */
 #bmc-eibor{ font-family:inherit; color:#202432; -webkit-font-smoothing:antialiased; }
